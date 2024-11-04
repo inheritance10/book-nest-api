b2f9bd132ac98a5a6604056d8784ce5679bf2a9e
Ali cebeci <103434459+inheritance10@users.noreply.github.com>
2024-11-05 00:22:23 +0300

    update swagger setup

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new LoggingInterceptor());
   const config = new DocumentBuilder()
-    .setTitle('Book service API Documentation')
-    .setDescription('Book service API Documentation')
+    .setTitle('Book Nest API')
+    .setDescription('API documentation for the Book Nest application')
     .setVersion('1.0')
     .addBearerAuth()
+    .setBasePath('/api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  });
+  SwaggerModule.setup('/api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
