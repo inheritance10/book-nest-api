b1fbbc14f1d6fbd290cc959e5cffec8b8c6485bd
Ali cebeci <103434459+inheritance10@users.noreply.github.com>
2024-11-05 00:53:10 +0300

    update swagger ui

--- src/main.ts
+++ src/main.ts
@@ -20,15 +20,6 @@ async function bootstrap() {
     swaggerOptions: {
       persistAuthorization: true,
     },
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-    ],
-    customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-    ],
   });
 
   await app.listen(process.env.PORT ?? 3000);
